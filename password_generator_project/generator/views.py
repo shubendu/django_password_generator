29769561bbb43cca5a73c85198e5a16fe7e1e000
shubendu <biswasshubendu4@gmail.com>
2021-06-17 16:10:58 +0530

    adding bootstrap

--- password_generator_project/generator/views.py
+++ password_generator_project/generator/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = int(request.GET.get('length',14))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%&*'))
+
+    if request.GET.get('number'):
+        characters.extend(list('1234567890'))
+
+    thepassword = ''
+    for i in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html',{'password':thepassword})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
